11ae2fe76e566288396e934dfc09c7514e127ef2
julianhumblet <julianhumbletp@outlook.com>
2024-04-10 22:39:34 +0200

    Modified database functionality

--- database/database.go
+++ database/database.go
@@ -5,4 +5,10 @@ type UserLogin struct {
 	Password string `json:"password"`
 }
 
+type UserRegistration struct {
+	Username         string `json:"username"`
+	Password         string `json:"password"`
+	RegistrationCode string `json:"registrationcode"`
+}
+
 // ADD DATABASE CONNECTION
