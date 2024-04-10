d0306ebc02b025440c2244f40f0cd5ddb4cd7374
julianhumblet <julianhumbletp@outlook.com>
2024-04-10 22:23:05 +0200

    Modified main file to needs

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"GymTracker/configfile"
 	"GymTracker/logfile"
+	"GymTracker/webserver"
 	"fmt"
+	"log"
+	"os"
 )
 
 func init() {
 
 	// Set the desired configuration settings here
 	pathLogfile := "./logfile.log"
+	pathConfigfile := "./config.json"
 
 	fmt.Println("Initializing application...")
 
@@ -16,9 +21,24 @@ func init() {
 	_, err := logfile.InitLogfile(pathLogfile)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Initialize configfile
+	err = configfile.InitConfigfile(pathConfigfile)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
 	}
 }
 
 func main() {
 
+	fmt.Println("Application has initialized succesfully.")
+	fmt.Println("Application is now starting.")
+
+	portWebserver := configfile.Configuration.Webserver.Port
+
+	// Start the webserver
+	webserver.StartWebserver(portWebserver)
 }
