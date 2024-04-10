b4dae0c67a4b1d1c3a1157c6e9429ff4b0ff8e62
julianhumblet <julianhumbletp@outlook.com>
2024-04-10 22:20:35 +0200

    Added webserver functionality

---
+++ webserver/webserver.go
@@ -0,0 +1,17 @@
+package webserver
+
+// Define constant values
+const (
+	invalidRequestBody      = "Invalid request body"
+	internalError           = "Internal server error"
+	unauthorized            = "Unauthorized"
+	usernameExists          = "Username already exists"
+	invalidCredentials      = "Invalid credentials"
+	methodNotAllowed        = "Method not allowed"
+	registrationCodeInvalid = "Registration code is not valid"
+)
+
+func StartWebserver(port int) {
+
+	
+}
