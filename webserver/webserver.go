f846d7da87c1bf5edb8309b420b224f04a679884
julianhumblet <julianhumbletp@outlook.com>
2024-04-10 22:35:19 +0200

    Modified webserver functionality

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,5 +1,11 @@
 package webserver
 
+import (
+	"GymTracker/database"
+	"encoding/json"
+	"net/http"
+)
+
 // Define constant values
 const (
 	invalidRequestBody      = "Invalid request body"
@@ -11,7 +17,47 @@ const (
 	registrationCodeInvalid = "Registration code is not valid"
 )
 
+// MODIFY API KEY FUNCTIONALITY
+
+// Valid API keys
+var validAPIKeys = map[string]bool{
+	"test": true,
+}
+
+// Function to check if the given API key is valid
+func isAPIKeyValid(apiKey string) bool {
+
+	return validAPIKeys[apiKey]
+}
+
 func StartWebserver(port int) {
 
-	
+	http.HandleFunc("/login", loginHandler)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Check if the used method is post
+	if r.Method != http.MethodPost {
+		http.Error(w, methodNotAllowed, http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Check if the given API key is valid
+	apiKey := r.Header.Get("Authorization")
+	if !isAPIKeyValid(apiKey) {
+		http.Error(w, unauthorized, http.StatusUnauthorized)
+		return
+	}
+
+	// Insert post data into UserLogin struct
+	var user database.UserLogin
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, invalidRequestBody, http.StatusBadRequest)
+		return
+	}
+
+	// ADD CREDENTIAL CHECK IN DATABASE
+	// IF CORRECT REDIRECT TO DASHBOARD
 }
