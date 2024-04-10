c9c03ef1c7571078c96b6a0a3b0f9a7bf50a0f70
julianhumblet <julianhumbletp@outlook.com>
2024-04-10 22:50:53 +0200

    Modified database functionality

--- database/database.go
+++ database/database.go
@@ -1,5 +1,12 @@
 package database
 
+import (
+	"GymTracker/configfile"
+	"database/sql"
+	"fmt"
+	"strconv"
+)
+
 type UserLogin struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -11,4 +18,23 @@ type UserRegistration struct {
 	RegistrationCode string `json:"registrationcode"`
 }
 
-// ADD DATABASE CONNECTION
+// MODIFY TO DATABASE PROVIDER
+func OpenDBCon() (*sql.DB, error) {
+
+	dbCreds := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		configfile.Configuration.Database.Username, configfile.Configuration.Database.Password,
+		configfile.Configuration.Database.Host, strconv.Itoa(configfile.Configuration.Database.Port),
+		configfile.Configuration.Database.DatabaseName)
+
+	dbCon, err := sql.Open("mysql", dbCreds)
+	if err != nil {
+		return nil, fmt.Errorf("error: opening database connection has failed: %s", err)
+	}
+
+	err = dbCon.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("error: database connection test (ping) has failed: %s", err)
+	}
+
+	return dbCon, nil
+}
